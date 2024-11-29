602d48929fcc28bbf064dd6b8b1d4a25c252a033
nikhila260 <nikhilamariam08@gmail.com>
2024-11-29 21:54:24 +0530

    first program

---
+++ day1/6.py
@@ -0,0 +1,4 @@
+name="Alice"
+age=30
+txt=f"My name is {name}, and I am {age} years old."
+print(txt)
---
+++ day1/7.py
@@ -0,0 +1,6 @@
+a=10
+print("10 as string:",a)
+b=3
+print("3.5 as integer:",b)
+c=25
+print('"25" as integer:',c)
